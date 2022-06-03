e5cc89c989410d25852e211cbf6bc3c204e47921
Anna Cho <loveablessing@gmail.com>
2022-06-02 20:16:28 -0400

    commit tree 4dab3115ba2eb66c8dc87489985ea5a66758af8a

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let newTitles = tutorials.map((string) => string.split(' ')).map((words) => words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
+  return newTitles
 }
+
+
+
+titleCased()
